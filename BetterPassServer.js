ac62b78c7d8c78a8f6e33d479fc8098757f54478
Robert Long <robert@robertlong.me>
2013-06-15 20:54:46 -0700

    Fixed clubs and various other bugs.

--- BetterPassServer.js
+++ BetterPassServer.js
@@ -13,7 +13,7 @@ app.set('view engine', 'html');
 app.use(app.router);
 
 app.use(function(req, res, next){
-  res.send(500, 'Page not found!');
+  res.send(404, 'Page not found!');
 });
 
 app.use(function(err, req, res, next){
@@ -43,6 +43,9 @@ app.get('/api/classes/:id', function(req, res, next){
    DB.query('SELECT * FROM Classes Classes a, Courses b WHERE a.course = b.id AND id = ?', req.params.id, function(err, classes) {
       if(err) {
          return next(err);
+      }
+      if(classes.length == 0) {
+         res.send(404, 'Item not found!');
       }
       res.send(classes[0]);
    });
@@ -81,6 +84,9 @@ app.get('/api/degrees/:id', function(req, res, next){
       if(err) {
          return next(err);
       }
+      if(degrees.length == 0) {
+         res.send(404, 'Item not found!');
+      }
       res.send(degrees[0]);
    });
 });
@@ -111,6 +117,9 @@ app.get('/api/courses/:id', function(req, res, next){
       if(err) {
          return next(err);
       }
+      if(courses.length == 0) {
+         res.send(404, 'Item not found!');
+      }
       res.send(courses[0]);
    });
 });
@@ -129,12 +138,18 @@ app.get('/api/teachers/:id', function(req, res, next){
       if(err) {
          return next(err);
       }
+      if(teachers.length == 0) {
+         res.send(404, 'Item not found!');
+      }
       res.send(teachers[0]);
    });
 });
 
 app.get('/api/teachers/:id/classes', function(req, res){
    DB.query('SELECT * FROM Classes WHERE id IN (SELECT id FROM Classes WHERE tId = ?)', req.params.id, function(err, teachers) {
+      if(err) {
+         return next(err);
+      }
       res.send(teachers);
    });
 });
@@ -152,6 +167,9 @@ app.get('/api/terms/:id', function(req, res, next){
    DB.query('SELECT * FROM Terms WHERE id = ?', req.params.id, function(err, terms) {
       if(err) {
          return next(err);
+      }
+      if(terms.length == 0) {
+         res.send(404, 'Item not found!');
       }
       res.send(terms[0]);
    });
@@ -171,15 +189,8 @@ app.get('/api/departments/:id', function(req, res, next){
       if(err) {
          return next(err);
       }
-
-      res.send(departments[0]);
-   });
-});
-
-app.get('/api/departments/:id', function(req, res, next){
-   DB.query('SELECT * FROM Departments WHERE id = ?', req.params.id, function(err, departments) {
-      if(err) {
-         return next(err);
+      if(departments.length == 0) {
+         res.send(404, 'Item not found!');
       }
       res.send(departments[0]);
    });
@@ -230,48 +241,59 @@ app.get('/api/clubs/:id', function(req, res, next){
       if(err) {
          return next(err);
       }
+      if(clubs.length == 0) {
+         res.send(404, 'Item not found!');
+      }
       res.send(clubs[0]);
    });
 });
 
 app.get('/api/clubs/:id/edit', function(req, res, next){
-   var setParams = [];
-   var setStatement = "";
+   var setParams = {};
    
    if(req.query.name){
-      setStatement += " name = ?,";
-      setParams.push(req.query.name);
+      setParams.name = req.query.name;
    }
    if(req.query.website){
-      setStatement += " website = ?";
-      setParams.push(req.query.website);  
+      setParams.website = req.query.website;  
    }
 
-   DB.query('SELECT * FROM Clubs WHERE id = ?', req.params.id, function(err, clubs) {
+   var q = DB.query('SELECT * FROM Clubs WHERE id = ?', req.params.id, function(err, clubs) {
       if(err) {
          console.log(err);
          return next(err);
       }
-      var hashedPass = crypto.createHash('md5').update(req.query.adminToken).digest("hex");
-      var storedHash = clubs[0].adminToken;
-      
-      if(hashedPass == storedHash) {
-         if(setParams.length > 0) {
-            setParams.push(req.params.id);
-            DB.query('UPDATE Clubs SET' + setStatement + "WHERE id = ?", setParams, function(err, club) {
-               if(err) {
-                  console.log(err);
-                  return next(err);
-               }
-               res.send(club);
-            });   
+
+      if (clubs.length > 0) {
+         if(req.query.adminToken) {
+            var hashedPass = crypto.createHash('md5').update(req.query.adminToken).digest("hex");   
+            var storedHash = clubs[0].adminToken;
+         } else {
+            res.send("Invalid token");
+         } 
+         
+         if(hashedPass == storedHash) {
+            if(setParams.name || setParams.website) {
+               var q2 = DB.query('UPDATE Clubs SET ? WHERE id = ?', [setParams, req.params.id], function(err, club) {
+                  if(err) {
+                     console.log(err);
+                     return next(err);
+                  }
+                  res.send(club);
+               });
+               console.log(q2.sql);   
+            } else{
+               res.send("Invalid parameters");
+            }
          } else{
-            res.send("Invalid parameters");
-         }
-      } else{
-         res.send("Invalid token");
+            res.send("Invalid token");
+         }   
+      } else {
+         res.send(404, 'Item not found!');
       }
+      
    });
+   console.log(q.sql);
    
 });
 
